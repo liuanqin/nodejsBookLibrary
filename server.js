00d8cde020ad00dc528f3b5300c7d4e355cd1b16
Jason Liu <liukahn@hotmail.com>
2021-05-01 22:23:52 -0600

    book and author routes

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 } 
 
-
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false } ))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, 
@@ -23,6 +26,8 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))    
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 
 
